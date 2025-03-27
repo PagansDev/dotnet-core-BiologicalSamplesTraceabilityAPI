abe21012f3bda12d2a4f2e101b58ee1f96703d67
AlienUrbano <paulogabrielneves@hotmail.com>
2025-03-27 02:24:48 -0300

    feature: Db Migration

---
+++ BiologicalSamplesTraceabilityAPI/Migrations/20250327051529_AddTables.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BiologicalSamplesTraceabilityAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Routes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Acronym = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Type = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Est_arrive = table.Column<TimeOnly>(type: "time", nullable: true),
+                    isActive = table.Column<bool>(type: "bit", nullable: false),
+                    Created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated_at = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Routines",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Routines", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SampleTypes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Created_at = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Updated_at = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SampleTypes", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Checkpoints",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Position = table.Column<int>(type: "int", nullable: false),
+                    RoutineId = table.Column<int>(type: "int", nullable: true),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Checkpoints", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Checkpoints_Routines_RoutineId",
+                        column: x => x.RoutineId,
+                        principalTable: "Routines",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "SampleBatches",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    RouteGroupId = table.Column<int>(type: "int", nullable: true),
+                    Status = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    Shift = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ClientType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    ConditioningType = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    SampleTypeId = table.Column<int>(type: "int", nullable: false),
+                    Temperature = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    TraySize = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Driver = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_SampleBatches", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_SampleBatches_SampleTypes_SampleTypeId",
+                        column: x => x.SampleTypeId,
+                        principalTable: "SampleTypes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "BatchIdentifiers",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Code = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Print = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SampleBatchId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_BatchIdentifiers", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_BatchIdentifiers_SampleBatches_SampleBatchId",
+                        column: x => x.SampleBatchId,
+                        principalTable: "SampleBatches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "RouteGroups",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    SampleBatchId = table.Column<int>(type: "int", nullable: false),
+                    RouteId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_RouteGroups", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_RouteGroups_Routes_RouteId",
+                        column: x => x.RouteId,
+                        principalTable: "Routes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_RouteGroups_SampleBatches_SampleBatchId",
+                        column: x => x.SampleBatchId,
+                        principalTable: "SampleBatches",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Traceabilities",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BatchIdentifierId = table.Column<int>(type: "int", nullable: false),
+                    CheckpointId = table.Column<int>(type: "int", nullable: false),
+                    Timestamp = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UserId = table.Column<int>(type: "int", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Traceabilities", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Traceabilities_BatchIdentifiers_BatchIdentifierId",
+                        column: x => x.BatchIdentifierId,
+                        principalTable: "BatchIdentifiers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Traceabilities_Checkpoints_CheckpointId",
+                        column: x => x.CheckpointId,
+                        principalTable: "Checkpoints",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_BatchIdentifiers_SampleBatchId",
+                table: "BatchIdentifiers",
+                column: "SampleBatchId",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Checkpoints_RoutineId",
+                table: "Checkpoints",
+                column: "RoutineId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RouteGroups_RouteId",
+                table: "RouteGroups",
+                column: "RouteId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_RouteGroups_SampleBatchId",
+                table: "RouteGroups",
+                column: "SampleBatchId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SampleBatches_SampleTypeId",
+                table: "SampleBatches",
+                column: "SampleTypeId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Traceabilities_BatchIdentifierId",
+                table: "Traceabilities",
+                column: "BatchIdentifierId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Traceabilities_CheckpointId",
+                table: "Traceabilities",
+                column: "CheckpointId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "RouteGroups");
+
+            migrationBuilder.DropTable(
+                name: "Traceabilities");
+
+            migrationBuilder.DropTable(
+                name: "Routes");
+
+            migrationBuilder.DropTable(
+                name: "BatchIdentifiers");
+
+            migrationBuilder.DropTable(
+                name: "Checkpoints");
+
+            migrationBuilder.DropTable(
+                name: "SampleBatches");
+
+            migrationBuilder.DropTable(
+                name: "Routines");
+
+            migrationBuilder.DropTable(
+                name: "SampleTypes");
+        }
+    }
+}
